6649236928ee06af42633789790a130ffce1b95c
Hakan Lindestaf <hakan@lindestaf.com>
2024-12-10 17:47:36 -0600

    Improvements to use pipeline and channels

--- src/Client.cs
+++ src/Client.cs
@@ -1,40 +1,45 @@
 ﻿using System;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipelines;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
 using System.Threading.Channels;
-using System.Threading.Tasks;
 using HdrHistogram;
 
 namespace Haukcode.HighPerfComm;
 
-public abstract class Client<T, TResult> : IDisposable where T : SendData
+public abstract class Client<T, TPacketType> : IDisposable where T : SendData
 {
-    private readonly CancellationTokenSource shutdownCTS = new();
+    private readonly CancellationTokenSource senderCTS = new();
+    private CancellationTokenSource? receiverCTS;
     private readonly HdrHistogram.Recorder sendRecorder;
     private readonly HdrHistogram.Recorder ageRecorder;
     private readonly Channel<T> sendQueue;
+    private readonly int receiveBufferSize;
     private int queueItemCounter;
     private int droppedPackets;
     private int fullQueue;
     private long totalPackets;
     protected readonly ISubject<Exception> errorSubject;
-    private Task? receiveTask1;
-    private Task? receiveTask2;
+    private Task? receiveTask;
+    private Task? parserTask;
     private readonly Task sendTask;
     private readonly Stopwatch receiveClock = new();
     private readonly MemoryPool<byte> memoryPool = MemoryPool<byte>.Shared;
     private readonly Func<T> sendDataFactory;
-    private readonly Memory<byte> receiveBufferMem1;
-    private readonly Memory<byte> receiveBufferMem2;
+    private long objectsFromPipeline;
+    private long objectsIntoChannel;
+    private Pipe? receivePipeline;
 
-    public Client(Func<T> sendDataFactory, int receiveBufferSize = 20480)
+    public Client(Func<T> sendDataFactory, int packetSize)
     {
+        this.receiveBufferSize = packetSize + HeaderDataSize;
         this.sendDataFactory = sendDataFactory;
         this.sendQueue = Channel.CreateBounded<T>(new BoundedChannelOptions(10000)
         {
@@ -61,16 +66,20 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
 
         this.errorSubject = new Subject<Exception>();
 
-        var receiveBuffer = GC.AllocateArray<byte>(length: receiveBufferSize, pinned: true);
-        this.receiveBufferMem1 = receiveBuffer.AsMemory();
-
-        receiveBuffer = GC.AllocateArray<byte>(length: receiveBufferSize, pinned: true);
-        this.receiveBufferMem2 = receiveBuffer.AsMemory();
-
         this.sendTask = Task.Factory.StartNew(Sender, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap();
     }
 
-    public bool IsOperational => !this.shutdownCTS.IsCancellationRequested;
+    protected abstract ValueTask<(int ReceivedBytes, SocketReceiveMessageFromResult Result)> ReceiveData(Memory<byte> memory, CancellationToken cancelToken);
+
+    protected abstract ValueTask<int> SendPacketAsync(T sendData, ReadOnlyMemory<byte> payload);
+
+    protected abstract void InitializeReceiveSocket();
+
+    protected abstract void DisposeReceiveSocket();
+
+    protected abstract TPacketType? TryParseObject(ReadOnlyMemory<byte> buffer, double timestampMS, IPEndPoint sourceIP, IPAddress destinationIP);
+
+    public bool IsOperational => !this.senderCTS.IsCancellationRequested;
 
     public void Dispose()
     {
@@ -82,26 +91,18 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
     {
         if (disposing)
         {
-            this.shutdownCTS.Cancel();
+            this.senderCTS.Cancel();
 
             this.sendQueue.Writer.Complete();
 
-            if (this.receiveTask1?.IsCanceled == false)
-                this.receiveTask1?.Wait(5_000);
-            this.receiveTask1?.Dispose();
-
-            if (this.receiveTask2 != null)
-            {
-                if (this.receiveTask2?.IsCanceled == false)
-                    this.receiveTask2?.Wait(5_000);
-                this.receiveTask2?.Dispose();
-            }
+            StopReceive();
 
             if (this.sendTask?.IsCanceled == false)
                 this.sendTask?.Wait(5_000);
             this.sendTask?.Dispose();
         }
     }
+
     public IObservable<Exception> OnError => this.errorSubject.AsObservable();
 
     public SendStatistics GetSendStatistics(bool reset)
@@ -113,7 +114,6 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
         {
             DroppedPackets = this.droppedPackets,
             QueueLength = this.queueItemCounter,
-            //DestinationCount = this.usedDestinations.Count,
             FullQueue = this.fullQueue,
             TotalPackets = this.totalPackets,
             SendStats = sendStatsCopy,
@@ -124,7 +124,6 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
         {
             // Reset
             this.droppedPackets = 0;
-            //this.usedDestinations.Clear();
             this.fullQueue = 0;
             this.totalPackets = 0;
         }
@@ -132,11 +131,21 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
         return sendStatistics;
     }
 
-    protected abstract ValueTask<int> SendPacketAsync(T sendData, ReadOnlyMemory<byte> payload);
+    public ReceiveStatistics GetReceiveStatistics()
+    {
+        long delta1 = Interlocked.Read(ref this.objectsFromPipeline) - Interlocked.Read(ref this.objectsIntoChannel);
+
+        var receiveStatistics = new ReceiveStatistics
+        {
+            ObjectsInQueue1 = (int)delta1
+        };
+
+        return receiveStatistics;
+    }
 
     private async Task Sender()
     {
-        while (!this.shutdownCTS.IsCancellationRequested)
+        while (!this.senderCTS.IsCancellationRequested)
         {
             await foreach (var sendData in this.sendQueue.Reader.ReadAllAsync())
             {
@@ -175,7 +184,7 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
                     if (ex is System.Net.Sockets.SocketException)
                     {
                         // Network unreachable
-                        this.shutdownCTS.Cancel();
+                        this.senderCTS.Cancel();
                         break;
                     }
                 }
@@ -190,14 +199,44 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
 
     public double ReceiveClock => this.receiveClock.Elapsed.TotalMilliseconds;
 
-    public void StartReceive()
+    private void StartReceive()
     {
-        this.receiveTask1 ??= Task.Factory.StartNew(() => Receiver(false), CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap();
+        if (this.receiverCTS != null)
+            throw new Exception("Already running");
 
-        if (SupportsTwoReceivers)
-            this.receiveTask2 ??= Task.Factory.StartNew(() => Receiver(true), CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap();
+        InitializeReceiveSocket();
+
+        this.objectsFromPipeline = 0;
+        this.objectsIntoChannel = 0;
+
+        this.receiverCTS = new CancellationTokenSource();
+
+        this.receiveTask = Task.Factory.StartNew(Receiver, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap();
 
         this.receiveClock.Restart();
+    }
+
+    public void StopReceive()
+    {
+        this.receiverCTS?.Cancel();
+
+        if (this.receiveTask?.IsCanceled == false)
+            this.receiveTask?.Wait(5_000);
+        this.receiveTask?.Dispose();
+
+        if (this.parserTask?.IsCanceled == false)
+            this.parserTask?.Wait(5_000);
+        this.parserTask?.Dispose();
+
+        this.receiverCTS?.Dispose();
+
+        this.receiveTask = null;
+        this.receiverCTS = null;
+        this.parserTask = null;
+
+        this.receivePipeline = null;
+
+        DisposeReceiveSocket();
     }
 
     protected async ValueTask QueuePacket(int allocatePacketLength, bool important, Func<T, Memory<byte>, int> packetWriter)
@@ -244,42 +283,90 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
         }
     }
 
-    protected abstract ValueTask<(int ReceivedBytes, TResult Result)> ReceiveData1(Memory<byte> memory, CancellationToken cancelToken);
+    protected const int HeaderDataSize = 24;
 
-    protected abstract ValueTask<(int ReceivedBytes, TResult Result)> ReceiveData2(Memory<byte> memory, CancellationToken cancelToken);
-
-    protected abstract bool SupportsTwoReceivers { get; }
-
-    protected abstract void ParseReceiveData(ReadOnlyMemory<byte> memory, TResult result, double timestampMS);
-
-    private async Task Receiver(bool receiver2)
+    private void WriteSocketDataToBuffer(int receivedBytes, long timestampTicks, IPEndPoint remoteEndPoint, IPAddress destAddress, Span<byte> buffer)
     {
-        while (!this.shutdownCTS.IsCancellationRequested)
+        // Write the packet size
+        BinaryPrimitives.WriteInt32LittleEndian(buffer, receivedBytes);
+
+        int writePos = 4;
+        // Write timestamp
+        BinaryPrimitives.WriteInt64LittleEndian(buffer[writePos..], timestampTicks);
+        writePos += 8;
+
+        writePos += WriteIpAddress(buffer[writePos..], remoteEndPoint.Address);
+
+        // Socket port
+        BinaryPrimitives.WriteInt32LittleEndian(buffer[writePos..], remoteEndPoint.Port);
+        writePos += 4;
+
+        writePos += WriteIpAddress(buffer[writePos..], destAddress);
+
+#if DEBUG
+        if (writePos > HeaderDataSize)
+            throw new ArgumentOutOfRangeException("Invalid data");
+#endif
+    }
+
+    private int WriteIpAddress(Span<byte> buffer, IPAddress input)
+    {
+        byte[] sourceAddr = input.GetAddressBytes();
+        sourceAddr.CopyTo(buffer);
+
+        return sourceAddr.Length;
+    }
+
+    public void GetSocketData(ReadOnlySpan<byte> buffer, out int packetSize, out double timestampMS, out IPEndPoint source, out IPAddress destination)
+    {
+        packetSize = BinaryPrimitives.ReadInt32LittleEndian(buffer);
+
+        Debug.Assert(packetSize > 0 && packetSize < this.receiveBufferSize);
+
+        long timestampTicks = BinaryPrimitives.ReadInt64LittleEndian(buffer.Slice(4));
+        timestampMS = (double)timestampTicks / Stopwatch.Frequency * 1000;
+
+        source = new IPEndPoint(new IPAddress(buffer.Slice(12, 4)), BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(16, 4)));
+        destination = new IPAddress(buffer.Slice(20, 4));
+    }
+
+    private async Task Receiver()
+    {
+        var writer = this.receivePipeline!.Writer;
+
+        while (!this.receiverCTS!.IsCancellationRequested)
         {
             try
             {
-                (int ReceivedBytes, TResult Result) result;
-                Memory<byte> receiveBufferMem;
+                Memory<byte> memory = writer.GetMemory(this.receiveBufferSize);
 
-                if (receiver2)
-                {
-                    result = await ReceiveData2(this.receiveBufferMem1, this.shutdownCTS.Token);
-                    receiveBufferMem = this.receiveBufferMem1;
-                }
-                else
-                {
-                    result = await ReceiveData1(this.receiveBufferMem2, this.shutdownCTS.Token);
-                    receiveBufferMem = this.receiveBufferMem2;
-                }
+                var result = await ReceiveData(memory[HeaderDataSize..], this.receiverCTS.Token);
 
                 // Capture the timestamp first so it's as accurate as possible
-                double timestampMS = this.receiveClock.Elapsed.TotalMilliseconds;
+                long timestampTicks = this.receiveClock.ElapsedTicks;
 
-                if (result.ReceivedBytes > 0)
+                if (result.Result.RemoteEndPoint is not IPEndPoint remoteEndPoint ||
+                    remoteEndPoint.AddressFamily != AddressFamily.InterNetwork ||
+                    result.Result.PacketInformation.Address.AddressFamily != AddressFamily.InterNetwork)
                 {
-                    var readBuffer = receiveBufferMem[..result.ReceivedBytes];
+                    // Missing or not IPv4
+                    continue;
+                }
 
-                    ParseReceiveData(readBuffer, result.Result, timestampMS);
+                int receivedBytes = result.ReceivedBytes;
+                if (receivedBytes > 0)
+                {
+                    WriteSocketDataToBuffer(receivedBytes, timestampTicks, remoteEndPoint, result.Result.PacketInformation.Address, memory.Span);
+
+                    // Commit data to the pipe
+                    writer.Advance(receivedBytes + HeaderDataSize);
+
+                    Interlocked.Increment(ref this.objectsFromPipeline);
+
+                    FlushResult flushResult = await writer.FlushAsync(this.receiverCTS.Token);
+
+                    if (flushResult.IsCompleted)
+                        break;
                 }
             }
             catch (Exception ex)
@@ -292,10 +379,127 @@ public abstract class Client<T, TResult> : IDisposable where T : SendData
                 if (ex is System.Net.Sockets.SocketException)
                 {
                     // Network unreachable
-                    this.shutdownCTS.Cancel();
+                    this.receiverCTS.Cancel();
                     break;
                 }
             }
         }
+
+        // Signal that writing is complete
+        await writer.CompleteAsync();
+    }
+
+    public void StartRecordPipeline<TChannelType>(Channel<TChannelType> outChannel, Func<TPacketType, TChannelType?> transformer)
+    {
+        this.receivePipeline = new Pipe(new PipeOptions(pauseWriterThreshold: 10_000_000));
+
+        // Start the pipeline components (we leave it running once it's started)
+        StartReceive();
+
+        this.parserTask = Task.Factory.StartNew(async () =>
+        {
+            await ParseFromPipeAsync(this.receivePipeline.Reader, outChannel, transformer, CancellationToken.None);
+        }, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+    }
+
+    private async Task ParseFromPipeAsync<TChannelType>(PipeReader reader, Channel<TChannelType> outputChannel, Func<TPacketType, TChannelType?> transformer, CancellationToken cancellationToken)
+    {
+        async Task processData(ReadOnlyMemory<byte> data, double timestampMS, IPEndPoint sourceIP, IPAddress destinationIP)
+        {
+            try
+            {
+                var parsedObject = TryParseObject(data, timestampMS, sourceIP, destinationIP);
+                if (parsedObject != null)
+                {
+                    Interlocked.Increment(ref this.objectsIntoChannel);
+
+                    var transformedObject = transformer(parsedObject);
+
+                    if (transformedObject != null)
+                    {
+                        await outputChannel.Writer.WriteAsync(transformedObject, cancellationToken);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.errorSubject.OnNext(ex);
+            }
+        }
+
+        async Task<int> processBuffer(ReadOnlySequence<byte> buffer, int packetSize, double timestampMS, IPEndPoint sourceIP, IPAddress destinationIP)
+        {
+            if (buffer.Length >= HeaderDataSize + packetSize)
+            {
+                var packetSequence = buffer.Slice(HeaderDataSize, packetSize);
+
+                if (packetSequence.IsSingleSegment)
+                {
+                    await processData(packetSequence.First, timestampMS, sourceIP, destinationIP);
+                }
+                else
+                {
+                    var copyBuf = this.memoryPool.Rent((int)packetSequence.Length);
+                    packetSequence.CopyTo(copyBuf.Memory.Span);
+
+                    await processData(copyBuf.Memory, timestampMS, sourceIP, destinationIP);
+
+                    copyBuf.Dispose();
+                }
+
+                return HeaderDataSize + packetSize;
+            }
+
+            return 0;
+        }
+
+        while (true)
+        {
+            ReadResult result = await reader.ReadAsync(cancellationToken);
+            ReadOnlySequence<byte> buffer = result.Buffer;
+
+            while (buffer.Length >= HeaderDataSize)
+            {
+                // We have enough data to read the packet size and details
+                ReadOnlySequence<byte> header = buffer.Slice(0, HeaderDataSize);
+
+                int advanceBytes = 0;
+
+                if (header.IsSingleSegment)
+                {
+                    GetSocketData(header.First.Span, out int packetSize, out double timestampMS, out IPEndPoint sourceIP, out IPAddress destinationIP);
+
+                    advanceBytes = await processBuffer(buffer, packetSize, timestampMS, sourceIP, destinationIP);
+                }
+                else
+                {
+                    // Unlikely that we'll have multiple segments for 24 bytes, but could happen
+                    var copyBuf = this.memoryPool.Rent((int)header.Length);
+                    header.CopyTo(copyBuf.Memory.Span);
+
+                    GetSocketData(copyBuf.Memory.Span, out int packetSize, out double timestampMS, out IPEndPoint sourceIP, out IPAddress destinationIP);
+
+                    advanceBytes = await processBuffer(buffer, packetSize, timestampMS, sourceIP, destinationIP);
+
+                    copyBuf.Dispose();
+                }
+
+                if (advanceBytes == 0)
+                    // We don't have enough data yet
+                    break;
+
+                buffer = buffer.Slice(advanceBytes); // Advance buffer
+            }
+
+            // Indicate consumed bytes
+            reader.AdvanceTo(buffer.Start, buffer.End);
+
+            if (result.IsCompleted)
+                break;
+        }
+
+        await reader.CompleteAsync();
+
+        outputChannel.Writer.Complete();
     }
 }
---
+++ src/ReceiveStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HdrHistogram;
+
+namespace Haukcode.HighPerfComm;
+
+public class ReceiveStatistics
+{
+    public int ObjectsInQueue1 { get; set; }
+
+    public int ObjectsInQueue2 { get; set; }
+}
